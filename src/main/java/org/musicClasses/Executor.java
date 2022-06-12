038ea19aeda38a7b7dcbc238e3f63814c0a7076f
justbegginer <blackcloudsinthesky@protonmail.com>
2022-06-12 07:09:50 +0300

    remake MusicPlayer.java by making the music field a list make bean for list version of MusicPlayer.java

--- src/main/java/org/musicClasses/Executor.java
+++ src/main/java/org/musicClasses/Executor.java
@@ -3,10 +3,18 @@ package org.musicClasses;
 import org.musicClasses.MusicPlayer;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Executor {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext beans = new ClassPathXmlApplicationContext("context.xml");
         MusicPlayer musicPlayer = beans.getBean("musicPlayerSetterBean", MusicPlayer.class);
+        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getDuration());
+        System.out.println();
+        musicPlayer = beans.getBean("musicPlayerByListBean", MusicPlayer.class);
         musicPlayer.playMusic();
     }
 }
--- src/main/java/org/musicClasses/MusicPlayer.java
+++ src/main/java/org/musicClasses/MusicPlayer.java
@@ -1,23 +1,59 @@
 package org.musicClasses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private ArrayList<Music> music;
     private String name;
     private int duration;
 
     public MusicPlayer(Music music) {
-        this.music = music;
+        this.music = new ArrayList<>();
+        this.music.add(music);
+    }
+
+    public MusicPlayer(List<Music> arrayList){
+        this.music = new ArrayList<>();
+        for (int i = 0; i < arrayList.size(); i++) {
+            music.add(arrayList.get(i));
+        }
     }
 
     public MusicPlayer() {
     }
 
     public void playMusic() {
-        music.play();
+        for (int i = 0; i < music.size(); i++) {
+            music.get(i).play();
+        }
+    }
+
+    public void stopMusic() {
+        for (int i = 0; i < music.size(); i++) {
+            music.get(i).stop();
+        }
     }
 
     public void setMusic(Music music) {
-        this.music = music;
+        this.music = new ArrayList<>();
+
+        this.music.add(music);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 }
